9557f09cea664dac1083b38d375f2a0754eff844
Miguel Ñancucheo <mnancuch@gmail.com>
2022-10-24 13:55:11 -0400

    commit con HMTL-CSS-JS

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 // script
 
-let el_bot_coockie = document.getElementById('bot_coockie');
-console.log(el_bot_coockie);
+let el_bot_coockie = document.querySelector('#bot_coockie');
 
 el_bot_coockie.addEventListener('click', function() {
     let padre = el_bot_coockie.parentElement;
-    padre.remove()
-    console.log(padre);
+    padre.remove();
+})
 
+
+
+let el_carro= document.querySelector('.img_cart');
+
+el_carro.addEventListener('click', function() {
+    alert('Your Cart is empy!');
+})
+
+
+
+let ele_play = document.querySelector('#img_play');
+
+ele_play.addEventListener('mouseover', function () {
+    ele_play.src = 'images/assets/succulents-2.jpg'
+})
+
+ele_play.addEventListener('mouseout', function () {
+    ele_play.src = 'images/assets/succulents-1.jpg'
 })
 
